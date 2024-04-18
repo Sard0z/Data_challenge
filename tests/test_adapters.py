20871d2b92803703099dc18ef633cea225426f60
Diego Monsalve <diandre2003@hotmail.com>
2024-04-18 15:34:16 -0500

    solved tests

--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -6,45 +6,36 @@ class TestPostcodesAdapter(unittest.TestCase):
 
     @patch('infrastructure.adapters.requests.get')
     def test_get_postal_code_success(self, mock_get):
-        # Arrange
         adapter = PostcodesAdapter(api_url='https://api.postcodes.io')
         mock_response = Mock()
         mock_response.status_code = 200
         mock_response.json.return_value = {'result': [{'postcode': '12345'}]}
         mock_get.return_value = mock_response
 
-        # Act
         result = adapter.get_postal_code(latitude=52.5, longitude=3.7)
 
-        # Assert
         self.assertEqual(result, ['12345'])
 
 
     @patch('infrastructure.adapters.requests.get')
     def test_get_postal_code_no_result(self, mock_get):
-        # Arrange
         adapter = PostcodesAdapter(api_url='https://api.postcodes.io')
         mock_response = Mock()
         mock_response.status_code = 200
         mock_response.json.return_value = {}
         mock_get.return_value = mock_response
 
-        # Act
         result = adapter.get_postal_code(latitude=52.5, longitude=3.7)
 
-        # Assert
         self.assertIsNone(result)
 
     @patch('infrastructure.adapters.requests.get')
     def test_get_postal_code_request_exception(self, mock_get):
-        # Arrange
         adapter = PostcodesAdapter(api_url='https://api.postcodes.io')
         mock_get.side_effect = requests.RequestException()
 
-        # Act
         result = adapter.get_postal_code(latitude=52.5, longitude=3.7)
 
-        # Assert
         self.assertIsNone(result)
 
 if __name__ == '__main__':
